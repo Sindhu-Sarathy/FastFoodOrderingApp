1bd02f656dee39f233c41ce4fc2ad70a71619da4
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2026-03-16 08:34:54 +0100

    Fixed alignment issue

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 const CartItem = ({item,increaseQty,decreaseQty}) => {
     return (
         <div className='d-flex justify-content-between align-items-center border-bottom p-2'>
-           <h6>{item.name}</h6>
-           <div>
+            <div style={{flex: 1}}>
+                <h6>{item.name}</h6>
+            </div>
+           
+           <div  style={{flex: 1}}>
             <button className='btn btn-danger btn-sm' onClick={() => decreaseQty(item.id)}>-</button>
             <span className='mx-2'>{item.qty}</span>
             <button className='btn btn-success btn-sm' onClick={() => increaseQty(item.id)}>+</button>
             </div> 
-            <p>{item.price * item.qty}SEK</p>
+            <div  style={{flex: 1}}> <p>{item.price * item.qty}SEK</p></div>
+           
         </div>
     );
 };
